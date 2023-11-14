44ec216c0f5aef734831942555932462ea33c111
Eric Casanova <kilbane1407@gmail.com>
2023-11-14 14:37:25 -0300

    implementado endpoint getAll

--- src/controllers/plans.js
+++ src/controllers/plans.js
@@ -1,6 +1,6 @@
 import { test as testService } from "../services/openai.js";
 import { generateResult } from "../promptGenerator/promptGenerator.js";
-import { createPlan } from "../managers/plans.js";
+import { createPlan, getAll as get } from "../managers/plans.js";
 export const test = async (req, res) => {
   try {
     const { msg } = req.body;
@@ -18,6 +18,16 @@ export const generatePlan = async (req, res) => {
     const steps = await generateResult(msg);
     const newPlan = await createPlan({ user, msg, steps });
     res.status(200).json(newPlan);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const getAll = async (req, res) => {
+  try {
+    const user = req.user;
+    const plans = await get(user);
+    res.status(400).json(plans);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- src/managers/plans.js
+++ src/managers/plans.js
@@ -1,11 +1,36 @@
 import { database } from "../db/firestore.js";
 import { PLANS, USERS } from "../utils/constants/collections.js";
-export const createPlan = async ({ user, msg, steps }) => {
+import { Timestamp } from "firebase-admin/firestore";
+const getUserorCreate = async (user) => {
   const currentUser = await database.collection(USERS).doc(user.id).get();
   if (!currentUser.exists) {
-    await currentUser.ref.create({ id: user.id, email: user.email });
+    await currentUser.ref.create({
+      id: user.id,
+      email: user.email,
+      created: Timestamp.now().seconds,
+    });
   }
-  const newPlanRef = currentUser.ref.collection(PLANS).doc();
-  await newPlanRef.set({ id: newPlanRef.id, userMsg: msg, steps });
+  return currentUser.ref;
+};
+
+export const createPlan = async ({ user, msg, steps }) => {
+  const currentUser = await getUserorCreate(user);
+  const newPlanRef = currentUser.collection(PLANS).doc();
+  await newPlanRef.set({
+    id: newPlanRef.id,
+    userMsg: msg,
+    steps,
+    created: Timestamp.now().seconds,
+  });
   return (await newPlanRef.get()).data();
+};
+
+export const getAll = async (user) => {
+  const currentUser = await getUserorCreate(user);
+  const results = await currentUser
+    .collection(PLANS)
+    .select("created", "id", "userMsg")
+    .orderBy("created")
+    .get();
+  return results.docs.map((d) => d.data());
 };
